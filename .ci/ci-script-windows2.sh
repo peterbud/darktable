a5b565fb2fc1adfe688aae2a52c4c0c91d4a63cd
Peter Budai <peterbudai@hotmail.com>
2019-01-30 22:46:48 +0100

    Update ci-script-windows2.sh

--- .ci/ci-script-windows2.sh
+++ .ci/ci-script-windows2.sh
@@ -8,6 +8,7 @@ pacman --noconfirm -Suy
 
 pacman --noconfirm -S --needed \
     git \
+    mingw-w64-$MSYS2_ARCH-cmake \
     mingw-w64-$MSYS2_ARCH-exiv2 \
     mingw-w64-$MSYS2_ARCH-lcms2 \
     mingw-w64-$MSYS2_ARCH-lensfun \
